9d25a5cdb62e2d6e6d0430e59105eebef26da4dd
Wlad Neto <wladimirteixeira@yahoo.com.br>
2024-02-22 23:33:23 -0300

    feat: push image to dockerhub

--- run-copy-to-rinha.sh
+++ run-copy-to-rinha.sh
@@ -1,3 +1,5 @@
+docker push wladimirteixeiraneto/rinha:latest
+
 cp ./docker-compose.yml ../rinha-de-backend-2024-q1/participantes/wladneto/docker-compose.yaml
 cp ./README.md ../rinha-de-backend-2024-q1/participantes/wladneto/README.md
 cp ./nginx.conf ../rinha-de-backend-2024-q1/participantes/wladneto/nginx.conf
